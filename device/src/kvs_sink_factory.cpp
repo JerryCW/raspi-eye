d81dd2654af16641d8d8b16c44241e639289b1d9
JerryCW <wangc.jerry@me.com>
2026-04-13 18:07:50 +0800

    spec-23: set kvssink log-config to ERROR to suppress Producer SDK DEBUG spam

--- device/src/kvs_sink_factory.cpp
+++ device/src/kvs_sink_factory.cpp
@@ -86,6 +86,7 @@ GstElement* create_kvs_sink(
         "stream-name", kvs_config.stream_name.c_str(),
         "aws-region",  kvs_config.aws_region.c_str(),
         "restart-on-error", FALSE,
+        "log-config",  "log4cplus:ERROR",
         nullptr);
     std::string iot_cert = build_iot_certificate_string(aws_config);
     gst_util_set_object_arg(G_OBJECT(sink), "iot-certificate",
